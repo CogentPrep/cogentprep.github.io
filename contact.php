6ae71d294999c68fd3396c2c55149b7d79164500
Rohith Perumalla <RohithPerumalla@gmail.com>
2017-02-25 20:08:03 -0600

    Major Reconstruction -Updated to have better responsiveness
    Theme now adapted from OPIA

--- contact.php
+++ contact.php
@@ -1,8 +1,18 @@
-<html>
-<body>
+<?php
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+    $to = "cogentprep@gmail.com"; // ENTER YOUR E-MAIL ADDRESS
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "You have a message.";
 
-</body>
-</html>
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"message"} = "message";
+
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
